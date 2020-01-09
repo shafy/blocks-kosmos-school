8323e19d898a818007611596ddfa0e364b366bd4
Can Olcer <shafyy@gmail.com>
2020-01-09 17:20:38 +0000

    fixed z pos in snapping

--- scripts/snap_area.gd
+++ scripts/snap_area.gd
@@ -4,10 +4,10 @@ extends Area
 class_name SnapArea
 
 
-var snap_area_1_other_area : Area
+var snap_area_other_area : Area
 var snapped := false
-var z_difference := -1
-var x_difference := -1
+var z_difference := 1
+var x_difference := 1
 
 onready var parent_block := get_parent()
 
@@ -18,54 +18,50 @@ func is_class(type):
 
 
 func _ready():
-	connect("area_entered", self, "_on_Snap_Area_1_area_entered")
-	connect("area_exited", self, "_on_Snap_Area_1_area_exited")
+	connect("area_entered", self, "_on_Snap_Area_area_entered")
+	connect("area_exited", self, "_on_Snap_Area_area_exited")
 	
 	# we need to know where snap area is in relation to the parent block on z axis for snapping later on
-	if parent_block.transform.origin.z > transform.origin.z:
-		z_difference = 1
-	else:
+	if transform.origin.z < 0:
 		z_difference = -1
 		
 	# same for x
-	if parent_block.transform.origin.x > transform.origin.x:
-		x_difference = 1
-	else:
+	if transform.origin.x < 0:
 		x_difference = -1
 
 
 func _process(delta):
-	if snap_area_1_other_area and snap_area_1_other_area.is_class("SnapArea") and !parent_block.is_grabbed and !snapped and !snap_area_1_other_area.snapped:
-		var snap_area_1_other_area_parent = snap_area_1_other_area.get_parent()
+	if snap_area_other_area and snap_area_other_area.is_class("SnapArea") and !parent_block.is_grabbed and !snapped and !snap_area_other_area.snapped:
+		var snap_area_other_area_parent = snap_area_other_area.get_parent()
 		
-		if !snap_area_1_other_area_parent:
+		if !snap_area_other_area_parent:
 			return
 
-		if !snap_area_1_other_area_parent.is_class("BuildingBlock"):
+		if !snap_area_other_area_parent.is_class("BuildingBlock"):
 			return
 		
 		
 		# snap to position if other area is still in this area and not grabbed anymore
-		if !snap_area_1_other_area_parent.is_grabbed:
+		if !snap_area_other_area_parent.is_grabbed:
 			snapped = true
-			snap_area_1_other_area.snap_to_block(self)
-			snap_area_1_other_area = null
+			snap_area_other_area.snap_to_block(self)
+			snap_area_other_area = null
 
 
-func _on_Snap_Area_1_area_entered(area):
-	snap_area_1_other_area = area
+func _on_Snap_Area_area_entered(area):
+	snap_area_other_area = area
 
 
-func _on_Snap_Area_1_area_exited(area):
-	snap_area_1_other_area = null
+func _on_Snap_Area_area_exited(area):
+	snap_area_other_area = null
 
 
 # snaps this block to another block
 func snap_to_block(other_snap_area: Area):
 	# move to far position but in right direction
 	var far_pos = other_snap_area.global_transform.origin + Vector3(0, 0, 1000)
-	
 	parent_block.global_transform.origin = far_pos
+	
 	
 	# rotate it so that this z-vector is aligned with other areas
 	# z-vector, but in the opposite direction
@@ -74,6 +70,9 @@ func snap_to_block(other_snap_area: Area):
 	
 	# update parent building block's rotation
 	parent_block.global_transform.basis = look_at_transform.basis
+	
+	# rotate by local y transform also
+	parent_block.rotate_y(rotation.y)
 	
 	if z_difference < 0:
 		# rotate by 180° degrees
@@ -87,14 +86,9 @@ func snap_to_block(other_snap_area: Area):
 	var move_by_vec = Vector3(
 		other_block_extents.x + (other_snap_area.x_difference * other_snap_area_extents.x),
 		0,
-		other_snap_area_extents.z - this_block_extents.z + 0.02
+		other_snap_area_extents.z - this_block_extents.z + 0.1
 	)
-	
-	
 	var close_pos = other_snap_area.global_transform.origin + move_by_vec
-	
 	parent_block.global_transform.origin = close_pos
 	
 	snapped = true
-	
-	
