8b904b189fc76890f0b30c4f75a20dad50d3ba40
Can Olcer <shafyy@gmail.com>
2020-01-10 16:08:39 +0000

    snapping seems to work

--- scripts/snap_area.gd
+++ scripts/snap_area.gd
@@ -22,12 +22,12 @@ func _ready():
 	connect("area_exited", self, "_on_Snap_Area_area_exited")
 	
 	# we need to know where snap area is in relation to the parent block on z axis for snapping later on
-	if transform.origin.z < 0:
-		z_difference = -1
-		
-	# same for x
-	if transform.origin.x < 0:
-		x_difference = -1
+#	if transform.origin.z < -0.001:
+#		z_difference = -1
+#
+#	# same for x
+#	if transform.origin.x < 0:
+#		x_difference = -1
 
 
 func _process(delta):
@@ -44,6 +44,7 @@ func _process(delta):
 		# snap to position if other area is still in this area and not grabbed anymore
 		if !snap_area_other_area_parent.is_grabbed:
 			snapped = true
+			snap_area_other_area.snapped = true
 			snap_area_other_area.snap_to_block(self)
 			snap_area_other_area = null
 
@@ -54,14 +55,15 @@ func _on_Snap_Area_area_entered(area):
 
 func _on_Snap_Area_area_exited(area):
 	snap_area_other_area = null
+	print("exited")
+	snapped = false
 
 
 # snaps this block to another block
 func snap_to_block(other_snap_area: Area):
+	print("hihihi")
 	# move to far position but in right direction
-	var far_pos = other_snap_area.global_transform.origin + Vector3(0, 0, 1000)
-	parent_block.global_transform.origin = far_pos
-	
+	parent_block.global_transform.origin += other_snap_area.global_transform.basis.z.normalized() * 1000
 	
 	# rotate it so that this z-vector is aligned with other areas
 	# z-vector, but in the opposite direction
@@ -70,25 +72,19 @@ func snap_to_block(other_snap_area: Area):
 	
 	# update parent building block's rotation
 	parent_block.global_transform.basis = look_at_transform.basis
+	# rotate by 180° degrees
+	parent_block.rotate_y(PI)
 	
 	# rotate by local y transform also
-	parent_block.rotate_y(rotation.y)
-	
-	if z_difference < 0:
-		# rotate by 180° degrees
-		parent_block.rotate_y(PI)
+	parent_block.rotate_y(-rotation.y)
 	
 	# move to close pos
 	# assuming other area's has a CollisionShape child and parent has CollisionShape child
 	var this_block_extents = parent_block.get_node("CollisionShape").shape.extents
 	var other_snap_area_extents = other_snap_area.get_node("CollisionShape").shape.extents
 	var other_block_extents = other_snap_area_parent.get_node("CollisionShape").shape.extents
-	var move_by_vec = Vector3(
-		other_block_extents.x + (other_snap_area.x_difference * other_snap_area_extents.x),
-		0,
-		other_snap_area_extents.z - this_block_extents.z + 0.1
-	)
-	var close_pos = other_snap_area.global_transform.origin + move_by_vec
-	parent_block.global_transform.origin = close_pos
 	
-	snapped = true
+	var move_by_vec = other_snap_area.global_transform.origin - global_transform.origin
+	move_by_vec -= other_snap_area.global_transform.basis.z.normalized() * other_snap_area_extents.x * 2
+	parent_block.global_transform.origin += move_by_vec
+	
