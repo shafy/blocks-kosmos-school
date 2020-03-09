5c2d7930d36083726be05f9930aca83e5b0f7b60
Buesi <dorena@humbot.io>
2020-03-09 18:36:46 +0000

    added amprere and volt cubes logic

--- scripts/measure_point.gd
+++ scripts/measure_point.gd
@@ -9,6 +9,8 @@ var parent_block : BuildingBlock
 
 onready var measure_controller := get_node(global_vars.MEASURE_CONTR_PATH)
 onready var controller_system := get_node(global_vars.CONTROLLER_SYSTEM_PATH)
+onready var cube_ampere := $CubeAmpere
+onready var cube_volt := $CubeVolt
 
 enum MeasurePointType {CONNECTION, BLOCK}
 export (MeasurePointType) var measure_point_type = MeasurePointType.CONNECTION setget set_measure_point_type, get_measure_point_type
@@ -24,6 +26,7 @@ func get_connection_id():
 
 func set_measure_point_type(new_value):
 	measure_point_type = new_value
+	update_cube_visibility()
 
 
 func get_measure_point_type():
@@ -31,7 +34,6 @@ func get_measure_point_type():
 
 
 func _ready():
-	# hide by default
 	visible = false
 	if measure_point_type == MeasurePointType.BLOCK:
 		parent_block = get_parent()
@@ -40,6 +42,8 @@ func _ready():
 	measure_controller.connect("ammeter_selected", self, "_on_Measure_Controller_ammeter_selected")
 	measure_controller.connect("voltmeter_selected", self, "_on_Measure_Controller_voltmeter_selected")
 	controller_system.connect("controller_type_changed", self, "_on_Controller_System_controller_type_changed")
+	
+	update_cube_visibility()
 
 
 func _on_Measure_Controller_ammeter_selected():
@@ -65,3 +69,14 @@ func get_current() -> float:
 		return parent_block.current
 	else:
 		return 0.0
+
+
+# either shows ampere or volt cube based on measure point type
+func update_cube_visibility() -> void:
+	if measure_point_type == MeasurePointType.BLOCK:
+		cube_ampere.visible = true
+		cube_volt.visible = false
+	else:
+		cube_ampere.visible = false
+		cube_volt.visible = true
+	
